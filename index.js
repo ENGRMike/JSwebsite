cf17ae7db0c2df082a60bdee37bf5a4c903439be
ENGRMike <michael.s.bruins@gmail.com>
2018-06-04 20:26:49 -0400

    completed multi search ToDo: add header

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 // references to the tbody element, input field, and button
 var $tbody = document.querySelector("tbody");
-var $stateInput = document.querySelector("#state");
-var $searchBtn = document.querySelector("#search");
+var $searchBtn = document.querySelector("#searchBtn");
+var $searchDate = document.querySelector("#datetime");
+var $searchCity = document.querySelector("#city");
+var $searchState = document.querySelector("#state");
+var $searchCountry = document.querySelector("#country");
+var $searchShape = document.querySelector("#shape")
 
 // event listener for the search button
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// set filteredAddresses to data initally
-var filteredAddresses = data;
+// set filteredAliens to data initially
+var filteredAliens = data;
 
 function renderTable() {
     $tbody.innerHTML="";
 
-    for (var i = 0; i < filteredAddresses.length; i++) {
+    for (var i = 0; i < filteredAliens.length; i++) {
 
-        var address = filteredAddresses[i];
-        var fields = Object.keys(address);
+        var aliens = filteredAliens[i];
+        var fields = Object.keys(aliens);
+        var row = $tbody.insertRow(i);
 
-        var $row = $tbody.insertRow(i);
         for (var j = 0; j < fields.length; j++) {
-            var field = fields[j];
-            var $cell = $row.insertCell(j);
-            $cell.innerText = address[field];
+            var cell = row.insertCell(j);
+            cell.innerHTML = aliens[fields[j]];
         }
     }
 }
 
-function handleSearchButtonClick() {
-    var filterState = $stateInput.value.trim().toLowerCase();
+function handleSearchButtonClick(event) {
+    event.preventDefault();
+    
+    var filterDate = $searchDate.value.trim().toLowerCase();
+    var filterCity = $searchCity.value.trim().toLowerCase();
+    var filterState = $searchState.value.trim().toLowerCase();
+    var filterCountry = $searchCountry.value.trim().toLowerCase();
+    var filterShape = $searchShape.value.trim().toLowerCase();
 
-    filteredAddresses = data.filter(function(address) {
-        var addressData = address.state.toLowerCase();
+    filteredAliens = data.filter(function(aliens) {
+        var alienDate = aliens.datetime.substring(0, filterDate.length).toLowerCase();
+        var alienCity = aliens.city.substring(0, filterCity.length).toLowerCase();
+        var alienState = aliens.state.substring(0, filterState.length).toLowerCase();
+        var alienCountry = aliens.country.substring(0, filterCountry.length).toLowerCase();
+        var alienShape = aliens.shape.substring(0, filterShape.length).toLowerCase();
 
-        return addressData === filterState;
+        if (alienDate === filterDate && alienCity === filterCity && alienState === filterState && alienCountry === filterCountry && alienShape === filterShape) {
+            return true;
+        }
+        return false;
     });
     renderTable();
 }
